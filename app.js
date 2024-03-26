647ea4afb98f51d6516cd9834c924a5bea676452
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-03-26 16:42:19 +0200

    Add First Routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import router from './services/router.js'
 
 const app = express()
 
+
+app.use(router)
 app.get('/', (req, res) => res.send('<h1>MailService</h1>'));
 
 
---
+++ controllers/user.controllers.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler'
+
+const activateAccount = asyncHandler(async (req, res) => {
+    // const { email, firstname, lastname, activationToken, isActivated } = req.body
+
+
+    // res.send('email here')
+})
+
+export { activateAccount }
