a28d8f3d3e5a54e365d95264b478dda9644b5b92
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-05-05 18:13:13 +0300

    add helmet middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import router from './services/router.js';
+import helmet from 'helmet';
 
 const app = express();
 
 // Add urlencoded: true
 app.use(express.json());
 
+app.use(helmet());
 app.use(router);
 app.get('/', (req, res) => res.send('<h1>MailService</h1>'));
 
