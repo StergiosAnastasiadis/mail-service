8bad0ced239fa66703adae758ab54750e49e8f31
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-05-05 18:24:29 +0300

    install cors and import it

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import express from 'express';
 import router from './services/router.js';
 import helmet from 'helmet';
 import { corsOptions } from './constants/config.js';
+import cors from 'cors'
 
 const app = express();
 
-app.use(express.urlencoded({ extended }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(helmet());
